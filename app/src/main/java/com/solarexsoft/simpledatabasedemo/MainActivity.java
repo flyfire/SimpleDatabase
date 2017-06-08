22e5fa50787b9a67a7ba1b9a424b8f15d0f72b43
Solarex <rh.hou.work@gmail.com>
2017-06-08 08:36:35 +0800

    modify test case

--- app/src/main/java/com/solarexsoft/simpledatabasedemo/MainActivity.java
+++ app/src/main/java/com/solarexsoft/simpledatabasedemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.solarexsoft.simpledatabasedemo;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.solarexsoft.simpledatabasedemo.dbtest.User;
 import com.solarexsoft.simpledatabasedemo.dbtest.UserDao;
@@ -22,14 +23,88 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         File file = new File(Environment.getExternalStorageDirectory(), "solarex.db");
         mUserDao = DaoManager.getInstance(file).getDao(UserDao.class, User.class);
-        mUserDao.insert(new User("Solarex", 18));
-        User where = new User();
-        where.setName("Solarex");
-        User entity = new User("David", 100);
-        mUserDao.update(entity, where);
-        mUserDao.insert(new User("Solarex", 18));
-        //mUserDao.delete(entity);
-        List<User> users = mUserDao.query(where);
-        L.d(users.toString());
+        //mUserDao.insert(new User("Solarex", 18));
+//        User where = new User();
+//        where.setName("Solarex");
+//        User entity = new User("David", 100);
+//        mUserDao.update(entity, where);
+//        mUserDao.insert(new User("Solarex", 18));
+//        //mUserDao.delete(entity);
+//        List<User> users = mUserDao.query(where);
+//        L.d(users.toString());
     }
+
+
+    public void sInsert(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long before = System.currentTimeMillis();
+                User user = new User("Solarex", 18);
+                for (int i = 0; i < 1000; i++) {
+                    mUserDao.insert(user);
+                }
+                long after = System.currentTimeMillis();
+                L.d("Solarex_Insert: " + (after - before));
+            }
+        }).start();
+    }
+
+    public void gInsert(View view) {
+    }
+
+    public void sUpdate(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long before = System.currentTimeMillis();
+                User where = new User();
+                where.setAge(18);
+                User entity = new User("David", 100);
+                int result = mUserDao.update(entity, where);
+                long after = System.currentTimeMillis();
+                L.d("Solarex_Update: " + result + ", spend " + (after - before));
+            }
+        }).start();
+    }
+
+    public void gUpdate(View view) {
+
+    }
+
+    public void sDelete(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long before = System.currentTimeMillis();
+                User where = new User();
+                where.setName("Solarex");
+                int result = mUserDao.delete(where);
+                long after = System.currentTimeMillis();
+                L.d("Solarex_Delete: " + result + ", spend " + (after - before));
+            }
+        }).start();
+    }
+
+    public void gDelete(View view) {
+    }
+
+    public void sQuery(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long before = System.currentTimeMillis();
+                User where = new User();
+                where.setName("Solarex");
+                List<User> users = mUserDao.query(where);
+                long after = System.currentTimeMillis();
+                L.d("Solarex_Query: " + users.size() + ", spend " + (after - before));
+            }
+        }).start();
+    }
+
+    public void gQuery(View view) {
+    }
+
+
 }
